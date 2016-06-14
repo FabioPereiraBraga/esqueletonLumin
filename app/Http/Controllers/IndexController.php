791cdec67c7277d7a296eb296ade244cdeca2039
FabioPereiraBraga <fabio.pereirabraga@gmail.com>
2016-06-13 23:10:33 -0300

    primeiro limite

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 13/06/16
+ * Time: 22:32
+ */
+
+namespace App\Http\Controllers;
+
+
+class IndexController extends Controller
+{
+
+    public function view( )
+    {
+        return 'dfsdfsdfdsf';
+    }
+
+
+}
